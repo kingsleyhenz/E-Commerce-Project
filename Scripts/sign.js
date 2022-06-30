77d1f3eb18c4ff2e40c1e37ada686017878e8061
Onyeka Ikedinobi <ikedinobicruz7@gmail.com>
2022-06-30 15:10:19 +0100

    Login and Signup

--- Scripts/sign.js
+++ Scripts/sign.js
@@ -1,25 +1,42 @@
 document.querySelector("#regform").addEventListener("submit", (e) => {
   e.preventDefault();
  
+  console.log("here")
+
    const formData = new FormData(e.target);
  
    const fullname = formData.get("fullname");
    const email = formData.get("email");
    const password = formData.get("psw");
  
-   const user = { fname: fullname, eml: email, psw: password };
+   const user = { fullname, email, password };
  
-   if (localStorage.getItem("user") === null) {
-     localStorage.setItem("user", JSON.stringify(user));
- 
-     Swal.fire({
-       position: "top-end",
-       icon: "success",
-       // title: "Success",
-       text: "Your account has been created successfully",
-       showConfirmButton: false,
-       timer: 3000,
-     });
+   if (!checkUser(email)) {
+
+      if (addUser(user)){
+        Swal.fire({
+          toast: true,
+          position: "top-end",
+          icon: "success",
+          // title: "Success",
+          text: "Your account has been created successfully",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+        });
+      } else {
+        Swal.fire({
+          toast: true,
+          position: "top-end",
+          icon: "error",
+          // title: "Success",
+          text: "An error occurred",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+        });
+      }
+
  
      setTimeout(() => {
        window.history.replaceState(null, null, window.location.pathname);
@@ -29,12 +46,14 @@ document.querySelector("#regform").addEventListener("submit", (e) => {
  
    }else{
      Swal.fire({
+          toast: true,
          position: "top-end",
-         icon: "info",
+         icon: "error",
          // title: "Success",
          text: "An account already exists",
          showConfirmButton: false,
          timer: 3000,
+         timerProgressBar: true,
        });
    }
  });
